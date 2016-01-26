6e46c8d338fa0adf67cf3fbe7b8a534c72debb27
essykings <essyking@gmail.com>
2016-01-26 15:11:48 +0300

    second changes

--- str_reverse.py
+++ str_reverse.py
@@ -1,4 +1,13 @@
 def reverse(word):
    return (word[::-1])
 print reverse("hello")
-pritn reverse("andela")
+print reverse("andela")
+
+def reverse2(word):
+	s= list(word)
+    	for i in range(len(word)//2):
+        	temp= s[i]
+        	s[i]= s[len(word)-i-1]
+        	s[len(word)-i-1] = temp
+    	return "".join(s)
+print reverse2("hello")
